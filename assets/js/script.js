07c2db4b7530ef3019459af7f609301bf7037169
knlinm <kenannuhbabali@gmail.com>
2024-04-07 13:12:04 +0400

    bugs fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 document.getElementsByClassName("game-header__button")[1].addEventListener('click', function () {
     var loserButtons = []
+    var buttons = document.getElementsByClassName("game-tile")
+    var placebuttons = document.getElementsByClassName("btnbet");
+    var smButtons = document.getElementsByClassName("_sm")
+    var balance
+    var balancePrompt = prompt("Balans")
+    if(isNaN(Number(balancePrompt))==true){
+        location.reload()
+    }
+    else{
+        balance = Number(balancePrompt)
+    }
+    document.getElementById("account").innerText=`$${balance}`
+    var cash = 0.15
+    var multipler=1
     function getLooserButtons() {
         let count = prompt("Bombaların sayı");
         if (Number(count) < 2) {
@@ -13,90 +27,124 @@ document.getElementsByClassName("game-header__button")[1].addEventListener('clic
         }
     }
     getLooserButtons();
-    var buttons = document.getElementsByClassName("game-tile")
-    var placebuttons = document.getElementsByClassName("btnbet");
-    var smButtons = document.getElementsByClassName("_sm")
-    document.getElementById("money").value = '0.15'
+    document.getElementById("money").value = `₼${cash}`
+    document.getElementById("money").addEventListener('change', function () {
+        if (Number(document.getElementById("money").value).toFixed(2) > 170) {
+            cash = 170
+            document.getElementById("money").value = `₼${cash.toFixed(2)}`
+        }
+        else if (Number(document.getElementById("money").value.slice(1)).toFixed(2) < 0.15) {
+            cash = 0.15
+            document.getElementById("money").value = `₼${cash.toFixed(2)}`
+        }
+        else if (Number(document.getElementById("money").value.slice(1)).toFixed(2) > 170) {
+            cash = 170
+            document.getElementById("money").value = `₼${cash.toFixed(2)}`
+        }
+        else {
+            if (document.getElementById("money").value.includes("₼")) {
+                cash = Number(document.getElementById("money").value.slice(1)).toFixed(2)
+            }
+            else {
+                cash = Number(document.getElementById("money").value).toFixed(2)
+            }
+            document.getElementById("money").value = `₼${(cash * 100 / 100).toFixed(2)}`
+        }
+    })
     smButtons[0].addEventListener('click', function () {
-        document.getElementById("money").value = '0.15'
+        cash = 0.15
+        document.getElementById("money").value = `₼${cash.toFixed(2)}`
     })
     smButtons[1].addEventListener('click', function () {
-        if (document.getElementById("money").value == '0.15') {
-            console.log("error")
+        if (cash <= 0.15) {
+            cash = 0.15
         }
-        else if (Number(document.getElementById("money").value) <= 1) {
-            let a = (Math.floor((Number(document.getElementById("money").value) - 0.1) * 100) / 100)
-            if (a <= 0.15) {
-                document.getElementById("money").value = '0.15'
-            }
-            else {
-                document.getElementById("money").value = a.toString()
-            }
-        }
-        else if (Number(document.getElementById("money").value) > 1 && (Number(document.getElementById("money").value <= 10))) {
-            let a = (Math.floor((Number(document.getElementById("money").value) - 1) * 100) / 100)
+        else if (cash <= 1) {
+            cash = ((Math.floor((cash - 0.1) * 100)) / 100)
 
-            if (a <= 0.15) {
-                document.getElementById("money").value = '0.15'
+            if (cash <= 0.15) {
+                cash = 0.15
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
             else {
-                document.getElementById("money").value = a.toString()
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
         }
-        else if (Number(document.getElementById("money").value) >= 10 && (Number(document.getElementById("money").value) <= 100)) {
-            let a = (Math.floor((Number(document.getElementById("money").value) - 10) * 100) / 100)
+        else if (cash > 1 && cash <= 10) {
+            cash = ((Math.floor((cash - 1) * 100)) / 100)
 
-            if (a <= 0.15) {
-                document.getElementById("money").value = '0.15'
+            if (cash <= 0.15) {
+                cash = 0.15
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
             else {
-                document.getElementById("money").value = a.toString()
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
         }
-        else if (Number(document.getElementById("money").value) > 100) {
-            document.getElementById("money").value = '100.00'
+        else if (cash > 10 && cash <= 100) {
+            cash = ((Math.floor((cash - 10) * 100)) / 100)
+
+            if (cash <= 0.15) {
+                cash = 0.15
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
+            }
+            else {
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
+            }
         }
+        else if(cash>100||cash<=170){
+            cash = 100
+            document.getElementById("money").value = `₼${cash.toFixed(2)}`
+        }
+        
     })
     smButtons[2].addEventListener('click', function () {
-        if (document.getElementById("money").value == '0.15') {
-            document.getElementById("money").value = '0.20'
+        if (cash == 0.15) {
+            cash = 0.20
+            document.getElementById("money").value = `₼${cash.toFixed(2)}`
         }
-        else if (Number(document.getElementById("money").value) < 1) {
-            let a = (Math.floor((Number(document.getElementById("money").value) + 0.1) * 100) / 100)
+        else if (cash < 1) {
+            cash = ((Math.floor((cash + 0.1) * 100)) / 100)
 
-            if (a >= 170) {
-                document.getElementById("money").value = '170'
+            if (cash >= 170) {
+                cash = 170
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
             else {
-                document.getElementById("money").value = a.toString()
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
         }
-        else if (Number(document.getElementById("money").value) >= 1 && (Number(document.getElementById("money").value) < 10)) {
-            let a = (Math.floor((Number(document.getElementById("money").value) + 1) * 100) / 100)
+        else if (cash >= 1 && cash < 10) {
+            cash = ((Math.floor((cash + 1) * 100)) / 100)
 
-            if (a >= 170) {
-                document.getElementById("money").value = '170'
+            if (cash >= 170) {
+                cash = 170
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
             else {
-                document.getElementById("money").value = a.toString()
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
         }
-        else if (Number(document.getElementById("money").value) >= 10 && (Number(document.getElementById("money").value) < 100)) {
-            let a = (Math.floor((Number(document.getElementById("money").value) + 10) * 100) / 100)
+        else if (cash >= 10 && cash < 100) {
+            cash = ((Math.floor((cash + 10) * 100)) / 100)
 
-            if (a >= 170) {
-                document.getElementById("money").value = '170'
+            if (cash >= 170) {
+                cash = 170
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
             else {
-                document.getElementById("money").value = a.toString()
+                document.getElementById("money").value = `₼${cash.toFixed(2)}`
             }
         }
-        else if (Number(document.getElementById("money").value) >= 100) {
-            document.getElementById("money").value = '170.00'
+
+        else if (cash >= 100) {
+            cash = 170
+            document.getElementById("money").value = `₼${cash.toFixed(2)}`
         }
     })
     smButtons[3].addEventListener('click', function () {
-        document.getElementById("money").value = 170
+        cash = 170
+        document.getElementById("money").value = `₼${cash.toFixed(2)}`
     })
 
     function replace() {
@@ -721,22 +769,52 @@ document.getElementsByClassName("game-header__button")[1].addEventListener('clic
             for (let i = 0; i < buttons.length; i++) {
                 buttons[i].addEventListener('click', function () {
                     if (buttons[i].classList.contains("winner")) {
-                        placebuttons[0].innerHTML = `<div class="btn-bg _bg1"></div><div class="btn-bg _bg11"></div><div class="btn-new__border"><div class="btn-bg _bg2"></div><div class="btn-bg _bg22"></div><div class="btn-new__inner"><div class="btn-bg _bg1"></div><div class="btn-bg _bg11"></div><div class="btn-new__text"><div class="text-wrapper"><div class="text">Cash Out</div><div class="_small"><span class="_roboto">₼0.24</span></div></div><div class="indicator _cashout"></div></div></div></div>`
                         if (!buttons[i].classList.contains("_opened")) {
                             buttons[i].classList.add("_opened")
                             buttons[i].classList.add("_win")
+                            
+                            let count = document.getElementsByClassName("_win")
+                            multipler=Number.parseFloat(document.getElementsByClassName("game-history__item-text")[count.length-1].innerHTML.slice(1))
+                            for(let i=0;i<document.getElementsByClassName("game-history__item").length;i++){
+                                document.getElementsByClassName("game-history__item")[i].classList.remove("_active")
+                            }
+                            document.getElementsByClassName("game-history__item")[count.length-1].classList.add("_active");
+                            if(count.length>=5){
+                                document.getElementsByClassName("game-history__inner-container")[0].scrollBy(78,0)
+                            }
+                            console.log(multipler)
+                            console.log(cash)
+                            
                             buttons[i].classList.add("_active")
                             let diamond = document.createElement("div")
                             diamond.classList.add("diamond")
                             let array = document.getElementsByClassName("game-tile__inner")
                             array[i].appendChild(diamond)
-                            placebuttons[0].classList.remove("_placebet")
-                            placebuttons[0].classList.remove("_cancel")
-                            placebuttons[0].classList.add("_cashout")
-                            placebuttons[1].classList.remove("_placebet")
-                            placebuttons[1].classList.remove("_cancel")
-                            placebuttons[1].classList.add("_cashout")
+
+
+
                             document.querySelector(".text-wrapper .text").innerHTML = "Cash out"
+                            if (!placebuttons[1].classList.contains("_cashout")) {
+                                placebuttons[1].classList.add("_cashout")
+                            }
+                            if (placebuttons[1].classList.contains("_placebet")) {
+
+                                placebuttons[1].classList.remove("_placebet")
+                            }
+                            if (placebuttons[1].classList.contains("_cancel")) {
+                                placebuttons[1].classList.remove("_cancel")
+                            }
+                            if (placebuttons[0].classList.contains("_placebet")) {
+                                placebuttons[0].classList.remove("_placebet")
+                            }
+                            if (placebuttons[0].classList.contains("_cancel")) {
+                                placebuttons[0].classList.remove("_cancel")
+
+                            }
+                            if (!placebuttons[0].classList.contains("_cashout")) {
+                                placebuttons[0].classList.add("_cashout")
+
+                            }
 
                         }
                     }
@@ -782,20 +860,23 @@ document.getElementsByClassName("game-header__button")[1].addEventListener('clic
                     }
                 }
             }
-            setTimeout(function () {
-                location.reload()
-            }, 1500)
+            // setTimeout(function () {
+            //     location.reload()
+            // }, 1500)
             document.getElementsByClassName("table-holder")[0].innerHTML += `
-        <div class="win-screen">
-        <div class="win-screen__head">Sən qalibsən!</div>
-        <div class="win-screen__content">
-        <div class="win-screen__content-payout" style="font-size: 23px;">₼0.24</div>
-        <div class="win-screen__content-footer">
-        <div class="win-screen__content-text">Çarpan</div>
-        <div class="win-screen__content-odd">x1.08</div>
-        </div>
-        </div>
-        </div>`
+            <div class="win-screen">
+            <div class="win-screen__head">Sən qalibsən!</div>
+            <div class="win-screen__content">
+            <div class="win-screen__content-payout" style="font-size: 23px;">${((Math.floor((cash*multipler*100)))/100).toFixed(2)}</div>
+            <div class="win-screen__content-footer">
+            <div class="win-screen__content-text">Çarpan</div>
+            <div class="win-screen__content-odd">x${multipler}</div>
+            </div>
+            </div>
+            </div>`
+            winnedMoney=((Math.floor((cash*multipler*100)))/100).toFixed(2)
+            winBalance=balance+Number(winnedMoney)
+            document.getElementById("account").innerText=`$${winBalance.toFixed(2)}`
             placebuttons[0].classList.remove("_cancel")
             placebuttons[0].classList.add("_placebet")
             placebuttons[1].classList.remove("_cancel")
